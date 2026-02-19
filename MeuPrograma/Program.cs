7f1ffc6eee7afc9c7f52bd4e265c9df10a0fe49f
[João Nogueira] <[joaonogueira.art@gmail.com]>
2026-02-19 15:15:28 +0000

    Add comment

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,12 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// This program says Hello world and Bye World
+    /// </summary>
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
-            Console.WriteLine("Bye world");
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Bye world!");
         }
     }
 }
