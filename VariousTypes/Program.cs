be4b261939b53bea12a00e3053a6b191231e4b34
[João Nogueira] <[joaonogueira.art@gmail.com]>
2026-02-19 15:32:36 +0000

    Add interager variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            sbyte int1 = 2;
+            short int2 = 4;
+            int int3 = 6;
+            long int4 = 8;
+
+            Console.WriteLine(int1);
+            Console.WriteLine(int2);
+            Console.WriteLine(int3);
+            Console.WriteLine(int4);
+        }
+    }
+}
